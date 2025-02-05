53c75cefe267966e832424d93b143dc00b4cdb76
DESKTOP-41A5A1D\Tou Chang <15tochang1@gmail.com>
2025-02-05 17:35:02 -0600

    no message

--- ClusterSystem/ClusterBackendAPI/obj/Debug/net9.0/ClusterBackendAPI.AssemblyInfo.cs
+++ ClusterSystem/ClusterBackendAPI/obj/Debug/net9.0/ClusterBackendAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClusterBackendAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a430a5a69f4ba3fd76491f3fdbd1aa6efbb472d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717b9630f0d783274cf85bdf71dde3d43040a2a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClusterBackendAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClusterBackendAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
